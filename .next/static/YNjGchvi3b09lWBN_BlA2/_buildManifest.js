18afe16f1985e11c4420fd4a52f1c39d70e7379c
Chris Fox <chris.b.fox1@gmail.com>
2022-01-12 08:54:54 -0500

    remove hardhat

--- .next/static/YNjGchvi3b09lWBN_BlA2/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/157-2e3e5b4e929e22642ae3.js","static/chunks/pages/index-185e0c3f00f4ebe9df51.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
